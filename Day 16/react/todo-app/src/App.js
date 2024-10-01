53b625c5ef59fae20353e0807fbaf8bec0e76a1f
jospen-kah <ngumkahjospen@gmail.com>
2024-10-01 09:00:39 +0100

    todo

--- Day 16/react/todo-app/src/App.js
+++ Day 16/react/todo-app/src/App.js
@@ -1,13 +1,1 @@
 
-import './App.css';
-import TodoList from './components/greet'
-
-function App() {
-  return (
-    <div className="App">
-      < TodoList  name="Sha" />
-    </div>
-  );
-}
-
-export default App;
---
+++ Day 16/todo/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TodoList from './components/greeting'
+
+function App() {
+  return (
+    <div className="App">
+      < TodoList  name="Sha" />
+    </div>
+  );
+}
+
+export default App;
