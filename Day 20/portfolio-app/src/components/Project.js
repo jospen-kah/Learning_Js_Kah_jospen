723c6b6af2900195d7db086198f945e433076fec
jospen-kah <ngumkahjospen@gmail.com>
2024-10-09 15:53:00 +0100

    portfolio

--- Day 20/portfolio-app/src/components/Project.js
+++ Day 20/portfolio-app/src/components/Project.js
@@ -1,20 +1,42 @@
 import React from "react";
 
 
-const Projects = () =>{
-return( 
-    <div className="project">
-   <h2>Projects</h2>
-   <div className="project-content">
-   <div className="each-project"></div>
-   <div className="each-project"></div>
-   <div className="each-project"></div>
-   <div className="each-project"></div>
-   <div className="each-project"></div>
-   <div className="each-project"></div>
-   </div>
-    </div>
-)
+const Projects = () => {
+    return (
+        <div className="project">
+            <h2>Projects</h2>
+            <div className="project-content">
+                <div className="each-project">
+                    <a href="https://learning-js-kah-jospen-p02ro0af9-jospenngumkgmailcoms-projects.vercel.app/">
+                        <img src="./to-do.PNG" alt="project1" />
+                    </a>
+                </div>
+                <div className="each-project">
+                <a href="https://learning-js-kah-jospen-p02ro0af9-jospenngumkgmailcoms-projects.vercel.app/">
+                        <img src="./to-do.PNG" alt="project1" />
+                    </a></div>
+                <div className="each-project">
+                <a href="https://joblisting-three.vercel.app/">
+                        <img src="./job-listing.PNG" alt="project1" />
+                    </a></div>
+                <div className="each-project">
+                <a href="https://learning-js-kah-jospen-p02ro0af9-jospenngumkgmailcoms-projects.vercel.app/">
+                        <img src="./to-do.PNG" alt="project1" />
+                    </a>
+                </div>
+                <div className="each-project">
+                <a href="https://learning-js-kah-jospen-p02ro0af9-jospenngumkgmailcoms-projects.vercel.app/">
+                        <img src="./to-do.PNG" alt="project1" />
+                    </a>
+                </div>
+                <div className="each-project">
+                <a href="https://joblisting-three.vercel.app/">
+                        <img src="./job-listing.PNG" alt="project1" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Projects;
--- Day 20/portfolio-app/src/components/Services.js
+++ Day 20/portfolio-app/src/components/Services.js
@@ -6,12 +6,24 @@ const Service = ()=>{
         <h1>Services</h1>
         <p>Services I offer</p>
         <div className='service-content'>
-            <div className='service1'>box1</div>
-            <div className='service1'>box1</div>
-            <div className='service1'>box1</div>
-            <div className='service1'>box1</div>
-            <div className='service1'>box1</div>
-            <div className='service1'>box1</div>
+            <div className='service1'>
+              <img src="./ui.png"  alt='ui'/>
+            </div>
+            <div className='service1'>
+              <img src="./web.jpeg"  alt='web'/>
+              </div>
+            <div className='service1'>
+            <img src="./ui.png"  alt='ui'/>
+            </div>
+            <div className='service1'>
+            <img src="./ui.png"  alt='ui'/>
+            </div>
+            <div className='service1'>
+            <img src="./ui.png"  alt='ui'/>
+            </div>
+            <div className='service1'>
+            <img src="./ui.png"  alt='ui'/>
+            </div>
         </div>
     </div>
   )
