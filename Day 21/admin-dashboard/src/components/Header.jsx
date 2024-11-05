d6375bcda46876b4e305c6d956f85101a2271f21
jospen-kah <ngumkahjospen@gmail.com>
2024-11-05 04:54:30 +0100

    dashboard

--- Day 21/admin-dashboard/src/components/Header.jsx
+++ Day 21/admin-dashboard/src/components/Header.jsx
@@ -1,38 +1,51 @@
+import  { useState } from 'react';
 import { Search, Bell, MailOpen, Logs, SunMedium, Grip, Menu } from 'lucide-react';
-// import ThemeContext  from './theme';
-
+import Navbar from "./Navbar";
 
 const Header = () => {
+    // State to control navbar visibility
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+    // Function to toggle navbar
+    const toggleNavbar = () => {
+        setIsNavbarOpen(!isNavbarOpen);
+    };
+
     return (
         <div className='main-header'>
             <div className="headers">
-                <div className='menu'>
-                    < Menu color="#6b7785" size={24} />
+                {/* Menu icon with onClick handler */}
+                <div className='menu' onClick={toggleNavbar}>
+                    <Menu color="#6b7785" size={24} />
                 </div>
+
                 <div className="search-bar">
                     <Search color="#6b7785" size={24} />
-                    <input onClick={{}} placeholder='Search' />
+                    <input placeholder='Search' />
                 </div>
+
                 <div className="bar-content">
-                    <div className="notification" ><Bell />
+                    <div className="notification"><Bell />
                         <div className='dot'></div>
                     </div>
                     <div className="log"><Logs />
-                        <div className='dot'></div></div>
-                        
-                    <div className="mail"  ><MailOpen />
-                    <div className='dot'></div></div>
-                    
-                    <div className="mode"  ><SunMedium /></div>
-                    {/* {theme === 'light' ? '🌞 Light Mode' : '🌙 Dark Mode'} */}
-                    <div className="profile-picture" >
-                        <img src='./user.jpeg'/>
+                        <div className='dot'></div>
                     </div>
-                    <div className="agenda" ><Grip /></div>
+                    <div className="mail"><MailOpen />
+                        <div className='dot'></div>
+                    </div>
+                    <div className="mode"><SunMedium /></div>
+                    <div className="profile-picture">
+                        <img src='./user.jpeg' alt="User Profile" />
+                    </div>
+                    <div className="agenda"><Grip /></div>
                 </div>
             </div>
-        </div>
-    )
-}
 
-export default Header
+            {/* Navbar component with conditional rendering */}
+            {isNavbarOpen && <Navbar />}
+        </div>
+    );
+};
+
+export default Header;
