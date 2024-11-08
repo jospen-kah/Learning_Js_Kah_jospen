6eeceacdbb63b25358ec881e1006192376b13eb5
jospen-kah <ngumkahjospen@gmail.com>
2024-11-08 13:18:13 +0100

    learn-node

--- learn-node/hello.js
+++ learn-node/hello.js
@@ -1,24 +1,15 @@
 const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const upper = require('upper-case');
+
 
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    const filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data){
-        if(err){
-            res.writeHead(404, {'content-type': 'text/html'});
-            return res.end("404 Not Found");
-        }
-
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    })
+  res.writeHead(200, {'Content-Type' : 'text/html '});
+  res.write(upper.upperCase("Hello World!"));
+  res.end();
 });
 
 
